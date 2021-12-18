080e1bc167e913e1edebafa38473ca8477e9c9ce
JfHarris <3374@holbertonschool.com>
2021-12-17 17:54:39 -0800

    Edit vars/pointers

--- new_node.c
+++ new_node.c
@@ -3,20 +3,20 @@
 /**
  * new_node - creates new node
  *@opcode: opcode
- *@sh: stack head
- *@input: input
+ *@stack: stack head
+ *@line_number: input
  *
  * Return: pointer to stack head.
  */
 
-stack_t *new_node(char *opcode, stack_t **sh, unsigned int input)
+stack_t *new_node(char *opcode, stack_t **stack, unsigned int line_number)
 {
 	stack_t *new;
 	int x;
 	char buff[1020];
 
 	if (strcmp(opcode, "push-\0") == 0 || strcmp(opcode, "push-\n") == 0)
-		free_push_err(input, *sh);
+		free_push_err(line_number, *stack);
 	new = malloc(sizeof(stack_t));
 
 	if (new == NULL)
@@ -32,23 +32,23 @@ stack_t *new_node(char *opcode, stack_t **sh, unsigned int input)
 		else
 		{
 			free(new);
-			free_push_err(input, *sh);
+			free_push_err(line_number, *stack);
 		}
 	}
 	buff[x] = '\0';
 	new->n = atoi(buff);
 	new->prev = NULL;
 
-	if ((*sh) == NULL)
+	if ((*stack) == NULL)
 	{
 		new->next = NULL;
-		(*sh) = new;
+		(*stack) = new;
 	}
 	else
 	{
-		new->next = (*sh);
-		(*sh)->prev = new;
-		(*sh) = new;
+		new->next = (*stack);
+		(*stack)->prev = new;
+		(*stack) = new;
 	}
-	return (*sh);
+	return (*stack);
 }
