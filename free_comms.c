166273268a9be8c9007c50a3f44c401b01c2fead
JfHarris <3374@holbertonschool.com>
2021-12-17 17:51:37 -0800

    Edit variable/pointers to them

--- free_comms.c
+++ free_comms.c
@@ -1,68 +1,68 @@
 #include "monty.h"
 
 /**
- * free_pint - pint error and free
- *@input: input from whence error arises
- *@sh: stack head
+ * free_push_err - pint error and free
+ *@line_number: input from whence error arises
+ *@stack: stack head
  *
  * Return: void
  */
 
-void free_push_err(unsigned int input, stack_t *sh)
+void free_push_err(unsigned int line_number, stack_t *stack)
 {
-	fprintf(stderr, "L%u: usage: push integer\n", input);
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
 	fclose(global.fd);
 	free(global.opcode);
-	free_all(sh);
+	free_all(stack);
 	exit(EXIT_FAILURE);
 }
 
 /**
  * free_mal_malloc - malloc error function
- *@sh: stack head
+ *@stack: stack head
  *
  * Return: void
  */
 
-void free_mal_malloc(stack_t *sh)
+void free_mal_malloc(stack_t *stack)
 {
 	fprintf(stderr, "Error: malloc failed\n");
 	fclose(global.fd);
 	free(global.opcode);
-	free_all(sh);
+	free_all(stack);
 	exit(EXIT_FAILURE);
 }
 
 /**
  * free_all - frees entire stack
- *@sh: stack head
+ *@stack: stack head
  *
  * Return: void.
  */
 
-void free_all(stack_t *sh)
+void free_all(stack_t *stack)
 {
 	stack_t *hold;
 
-	while (sh != NULL)
+	while (stack != NULL)
 	{
-		hold = sh;
-		sh = sh->next;
+		hold = stack;
+		stack = stack->next;
 		free(hold);
 	}
 }
 
 /**
  * free_gen_error - frees in case of general error
- *@sh: stack head
+ *@stack: stack head
  *
  * Return: void.
  */
 
-void free_gen_error(stack_t *sh)
+void free_gen_error(stack_t *stack)
 {
 	fclose(global.fd);
 	free(global.opcode);
-	free_all(sh);
+	free_all(stack);
 	exit(EXIT_FAILURE);
 }
